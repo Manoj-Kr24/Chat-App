af2254ba6d20089c8331631197ebac52d2f0d7c0
Manoj-Kr24 <kumarmanojmanjhi2406@gmail.com>
2025-03-02 23:38:52 +0530

    deployment code added

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const messageRoutes = require("./routes/messageRoutes");
 const colors = require("colors");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
+const path = require("path");
+
 dotenv.config();
 
 connectDB();
@@ -25,15 +27,21 @@ app.use("/api/chat", chatRoutes);
 
 app.use("/api/message", messageRoutes);
 
-// app.get("/api/chats", (req, res) => {
-//   // console.log(chats);
-//   res.send(chats);
-// });
+// --------------------------------Deployment --------------------------------
+const __dirname1 = path.resolve();
 
-// app.get("api/chats/:id", (req, res) => {
-//   const singleChat = chats.find((c) => c._id === req.params.id);
-//   res.send(singleChat);
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running ");
+  });
+}
+// --------------------------------Deployment --------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
